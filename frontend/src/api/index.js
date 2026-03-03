5d222030685bebc8f606e12c9df203858282f9b6
Cristian <cristiancv1994@gmail.com>
2026-03-03 18:25:55 -0400

    Cambiando el puerto del Banckend

--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://192.168.100.110:8000', // Adjust the base URL as needed
+    baseURL: 'http://192.168.100.110:8889', // Adjust the base URL as needed
     headers: {
         'Content-Type': 'application/json',
     },
